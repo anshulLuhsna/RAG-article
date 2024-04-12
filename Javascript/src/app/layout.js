05ee4532941f1f94a9558cbc08f4de7969b24325
anshulLuhsna <anshulkalbande@gmail.com>
2024-04-12 10:49:56 +0530

    fix: removed inter font

--- Javascript/src/app/layout.js
+++ Javascript/src/app/layout.js
@@ -2,7 +2,6 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Echo',
@@ -13,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <SpeedInsights />
-      <body className={inter.className}>
+      <body >
         {children}
         <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.6.347/pdf.min.js" integrity="sha512-Z8CqofpIcnJN80feS2uccz+pXWgZzeKxDsDNMD/dJ6997/LSRY+W4NmEt9acwR+Gt9OHN0kkI1CTianCwoqcjQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         </body>
